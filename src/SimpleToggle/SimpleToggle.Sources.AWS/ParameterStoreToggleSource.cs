67ebfd270861f8d3bb17108d960b59d860ab1b67
Miguel Laranjeira <Miguel@synthesis.co.za>
2021-04-12 13:03:00 +0200

    Added AWS Source Project

---
+++ src/SimpleToggle/SimpleToggle.Sources.AWS/ParameterStoreToggleSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using Microsoft.Extensions.Options;
+using SimpleToggle.Core;
+
+namespace SimpleToggle.Sources.AWS
+{
+    public class ParameterStoreToggleSource : IToggleSource
+    {
+        private readonly FeatureToggles toggles;
+        private readonly IAmazonSimpleSystemsManagement systemsManagement;
+
+        public ParameterStoreToggleSource(IAmazonSimpleSystemsManagement systemsManagement, IOptions<FeatureToggles> toggles)
+        {
+            this.toggles = toggles.Value;
+            this.systemsManagement = systemsManagement;
+        }
+
+        public async Task<bool> GetToggleValue(string toggleName)
+        {
+            if (!toggles.TryGetValue(toggleName, out var parameterName))
+            {
+                return false;
+            }
+
+            try
+            {
+                var response = await systemsManagement.GetParameterAsync(new GetParameterRequest() { Name = parameterName });
+                if (!bool.TryParse(response.Parameter.Value, out bool value))
+                {
+                    return false;
+                }
+
+                return value;
+            }
+            catch (ParameterNotFoundException)
+            {
+                return false;
+            }
+        }
+    }
+}
